e2eea010cdabca90d683b3da294230cad06b9153
Miniracle <artemyatsishchuk@gmail.com>
2022-10-20 23:16:46 +0300

    first commit

---
+++ src/components/Cubes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useStore } from '../hooks/useStore'
+import Cube from './Cube'
+
+export default function Cubes() {
+  const [cubes] = useStore((state)=> [
+    state.cubes
+  ])
+  return cubes.map(({key, pos, texture}) =>{
+    return <Cube key={key} position={pos} texture = {texture}/>;
+  })
+}
